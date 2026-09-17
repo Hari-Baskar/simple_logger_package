298cbaa816b3033b5b66732b7e6da0d70dc71a3a
arunkumar <arun@advisorkhoj.in>
2026-09-17 16:27:00 +0530

    package name updated

--- test/simple_logger_test.dart
+++ test/simple_logger_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:simple_logger/simple_logger.dart';
+import 'package:simple_logger_package/simple_logger.dart';
 
 void main() {
   test("Logger test", () {
